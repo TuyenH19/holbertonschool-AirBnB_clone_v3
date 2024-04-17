e66f605d9876b41883d7fade1a9708826c401a43
TuyenH <thtuyen199@gmail.com>
2024-04-17 14:33:47 +0000

    Task5- add endpoint to retrieve the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,30 @@
 
 from . import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """Return a JSON response indicating the status is OK."""
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', methods=['GET'])
+def count():
+    """Retrieve the number of each objects by type"""
+    object_counts = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(object_counts)

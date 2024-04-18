b7db00363a716d45fd0411f829f405f93f392781
TuyenH <thtuyen199@gmail.com>
2024-04-18 09:30:29 +0000

    Task7- create a new view State object to handle all default RESTFul API actions

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,4 +8,9 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # Importing views modules;
 # PEP8 will complain about wildcard import, but it's necessary here
+
+# Import the route defined in index
 from .index import *  # noqa: E402
+
+#import the route defined in states
+from .states import * # noqa: E402

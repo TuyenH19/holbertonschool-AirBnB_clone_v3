88ff8dcde921127a6f410e631b691997a2c33714
TuyenH <thtuyen199@gmail.com>
2024-04-19 14:26:03 +0000

    update cities routes to init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,8 +9,7 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 # Importing views modules;
 # PEP8 will complain about wildcard import, but it's necessary here
 
-# Import the route defined in index
+# Import the route defined in all files
 from .index import *  # noqa: E402
-
-# import the route defined in states
 from .states import *  # noqa: E402
+from .cities import *  # noqa: E402

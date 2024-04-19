638922704bf7c69fa4128a709b54bb84259eda34
Hache_David <hache.david@free.fr>
2024-04-19 09:47:51 +0000

    Try new import module for init folder

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""Create a Blueprint from flask and import views module"""
-
+"""Initialize Blueprint views"""
 from flask import Blueprint
 
-# Create a Blueprint named 'app_views'
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-# Importing views modules;
-# PEP8 will complain about wildcard import, but it's necessary here
-
-# Import the route defined in index
-from .index import *  # noqa: E402
-
-#import the route defined in states
-from .states import * # noqa: E402
+from api.v1.views.index import *
+from api.v1.views.cities import *
+from api.v1.views.states import *
